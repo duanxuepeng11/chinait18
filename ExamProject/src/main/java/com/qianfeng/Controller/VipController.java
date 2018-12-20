5065a0a9ec4932c8161698579e47d919f3a9ce29
dongxiucai <dongrunhua@163.com>
2018-12-20 15:51:29 +0800

    ddd

---
+++ ExamProject/src/main/java/com/qianfeng/Controller/VipController.java
@@ -0,0 +1,11 @@
+package com.qianfeng.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/vip")
+public class VipController {
+
+
+}
