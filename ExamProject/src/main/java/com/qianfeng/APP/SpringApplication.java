8c93576e5f80e5b6b6020cc07063aa446400aad1
duanxuepeng11 <39993348+duanxuepeng11@users.noreply.github.com>
2019-03-11 20:07:35 +0800

    Update SpringApplication.java

--- ExamProject/src/main/java/com/qianfeng/APP/SpringApplication.java
+++ ExamProject/src/main/java/com/qianfeng/APP/SpringApplication.java
@@ -12,7 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringApplication {
 //    com.qianfeng.APP.SpringApplication
     public static void main(String[] args) {
-        //3.11加入了一个注释
+        //3.11加入了一个注释2.0
         org.springframework.boot.SpringApplication.run(SpringApplication.class,args);
     }
 }
